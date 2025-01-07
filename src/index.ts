07da637608480e283c9fa409608f4a19729cc94d
oustn <kun.zeng.dev@gmail.com>
2025-01-07 15:09:39 +0800

    feat: Add preact&vite support

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,18 @@
-import { fromHono } from "chanfana";
-import { Hono } from "hono";
-import { TaskCreate } from "./endpoints/taskCreate";
-import { TaskDelete } from "./endpoints/taskDelete";
-import { TaskFetch } from "./endpoints/taskFetch";
-import { TaskList } from "./endpoints/taskList";
+import {fromHono} from "chanfana";
+import {Hono} from "hono";
+import {TaskCreate} from "./endpoints/taskCreate";
+import {TaskDelete} from "./endpoints/taskDelete";
+import {TaskFetch} from "./endpoints/taskFetch";
+import {TaskList} from "./endpoints/taskList";
 
 // Start a Hono app
-const app = new Hono();
+const app = new Hono<{
+  Bindings: Env
+}>();
 
 // Setup OpenAPI registry
 const openapi = fromHono(app, {
-	docs_url: "/",
+  docs_url: null,
 });
 
 // Register OpenAPI endpoints
@@ -18,6 +20,16 @@ openapi.get("/api/tasks", TaskList);
 openapi.post("/api/tasks", TaskCreate);
 openapi.get("/api/tasks/:taskSlug", TaskFetch);
 openapi.delete("/api/tasks/:taskSlug", TaskDelete);
+
+// Web
+app.get("/*", async (c) => {
+  if (c.env.ENVIRONMENT === 'dev') {
+    const url = new URL(c.req.raw.url)
+    url.port = c.env.VITE_PORT
+    return fetch(new Request(url, c.req.raw))
+  }
+  return c.env.ASSETS.fetch(c.req.raw)
+})
 
 // Export the Hono app
 export default app;
